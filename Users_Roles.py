68593a019cb95429b812285424cf47b36811c951
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-11-11 07:51:32 -0500

    Update Users_Roles.py

--- Users_Roles.py
+++ Users_Roles.py
@@ -7,4 +7,6 @@ streamlit.title('Air Canada Webpage')
 
 streamlit.header('Users & Roles')
 streamlit.text('USER: An identiy recognized by Snowflake, associated to Person or Program (Azure x Air Canada).\n')
-streamlit.text('ROLE: An entity to which privileges can be granted. Roles are assigned to users. Roles may also be assigned to roles, creating a role hierarchy.')
+streamlit.text('ROLE: An entity to which privileges can be granted.')
+streamlit.text('Roles are assigned to users.')
+streamlit.text('Roles may also be assigned to other roles, creating a role hierarchy.')
