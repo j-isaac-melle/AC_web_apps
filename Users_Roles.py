9492482c535968914addce1ea0aee7e687d07c08
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-11-11 07:53:22 -0500

    Update Users_Roles.py

--- Users_Roles.py
+++ Users_Roles.py
@@ -10,3 +10,7 @@ streamlit.text('USER: An identiy recognized by Snowflake, associated to Person o
 streamlit.text('ROLE: An entity to which privileges can be granted.')
 streamlit.text('    Roles are assigned to users.')
 streamlit.text('    Roles may also be assigned to other roles, creating a role hierarchy.')
+
+
+Streamlit.tittle('USERS:')
+
