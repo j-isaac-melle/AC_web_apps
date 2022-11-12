eca563dfce5bb4b3a20600590f15e94f165938db
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-11-11 22:35:12 -0500

    Update Users_Roles.py

--- Users_Roles.py
+++ Users_Roles.py
@@ -19,7 +19,5 @@ streamlit.header('Chck out our users:')
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake2"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
+my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.USERS")
 streamlit.text(my_data_row)
