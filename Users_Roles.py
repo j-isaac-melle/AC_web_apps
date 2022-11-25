55d18ce0ece3598dccdb50784f65b3a0a2d9c36b
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-11-25 16:33:34 -0500

    Update Users_Roles.py

--- Users_Roles.py
+++ Users_Roles.py
@@ -26,12 +26,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * FROM PC_RIVERY_DB.PUBLIC.USERS")
 myresult = my_cur.fetchall()
 
-#my_cur = my_cur.set_index('Name')
-#pick select list here for smothie creation: (list from index index declared)
-#manually adds avo and bana to list 
-#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-#hides the giant table 
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 streamlit.dataframe(myresult)
 
 
